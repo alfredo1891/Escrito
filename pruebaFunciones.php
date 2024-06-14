123426dd539992a56cfb4fd40d1a6e03bb72c50b
alfredo chiffone <alfredoacordate@gmail.com>
2024-06-14 19:03:24 -0300

    agregamos la funcion buscar x modelo

--- pruebaFunciones.php
+++ pruebaFunciones.php
@@ -9,3 +9,13 @@ function agregarProducto($arrayproductos,$nombre,$cantidad,$modelo){
 $arrayproductos = [];
 $arrayproductos= agregarProducto($arrayproductos,"Heladera","1","James");
 return $arrayusuarios;
+
+
+function buscarProductoporModelo($arrayproductos, $modelo) {
+    foreach ($arrayproductos as $arrayproductos) {
+        if ($arrayproductos['modelo'] == $modelo) {
+            return "Nombre: " . $arrayproductos['nombre'] . "<br>";
+        }
+    }
+    return "Producto no encontrado.<br>";
+}
